db24673823118a4c036dcaf966d5ad619f9d9cf2
seleb <sean.s.leblanc@gmail.com>
2015-05-08 19:58:36 -0400

    testing cpprest
    work in release but not debug?

--- Party Darling/Source/Party Darling/src/PD_TestScene.cpp
+++ Party Darling/Source/Party Darling/src/PD_TestScene.cpp
@@ -47,6 +47,15 @@
 #include <StringUtils.h>
 #include <CharacterUtils.h>
 
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+/*using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams*/
+
 
 PD_TestScene::PD_TestScene(Game * _game) :
 	Scene(_game),
@@ -257,6 +266,41 @@ PD_TestScene::PD_TestScene(Game * _game) :
 		me->setShader(shader, true);
 		childTransform->addChild(me);
 	}*/
+
+	auto fileStream = std::make_shared<concurrency::streams::ostream>();
+
+    // Open stream to output file.
+	pplx::task<void> requestTask = concurrency::streams::fstream::open_ostream(U("results.html")).then([=](concurrency::streams::ostream outFile){
+        *fileStream = outFile;
+
+        // Create http_client to send the request.
+		web::http::client::http_client client(U("http://www.bing.com/"));
+
+        // Build request URI and start the request.
+        web::uri_builder builder(U("/search"));
+        builder.append_query(U("q"), U("Casablanca CodePlex"));
+		utility::string_t test = builder.to_string();
+        return client.request(web::http::methods::GET, test);
+    })
+    // Handle response headers arriving.
+    .then([=](web::http::http_response response){
+        printf("Received response status code:%u\n", response.status_code());
+
+        // Write response body into the file.
+        return response.body().read_to_end(fileStream->streambuf());
+    })
+    // Close the file stream.
+    .then([=](size_t){
+        return fileStream->close();
+    });
+
+	
+    // Wait for all the outstanding I/O to complete and handle any exceptions
+    try{
+        requestTask.wait();
+    }catch (const std::exception &e){
+        printf("Error exception:%s\n", e.what());
+    }
 }
 
 void PD_TestScene::addThing(){
