69e25097402a0b3ec2d6fc589ae32bc802c4618f
thehetmanm <thehetmanm@hotmail.com>
2016-01-11 13:50:47 -0500

    Player Head Bobble and Jump Stuff

--- Party Darling/Source/Party Darling/src/Player.cpp
+++ Party Darling/Source/Party Darling/src/Player.cpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <Player.h>
 
 #include <node/NodeChild.h>
@@ -37,6 +36,9 @@
 
 #include <sweet/Input.h>
 
+#include <Animation.h>
+#include <Tween.h>
+
 Player::Player(glm::vec3  sPos, BulletWorld * _bulletWorld, MousePerspectiveCamera * _playerCamera) : 
 	playerCamera(_playerCamera),
 	NodeBulletBody(_bulletWorld),
@@ -53,17 +55,52 @@ Player::Player(glm::vec3  sPos, BulletWorld * _bulletWorld, MousePerspectiveCame
 	this->body->setFriction(1);
 	this->body->setAngularFactor(btVector3(0,1,0));
 	this->body->setLinearFactor(btVector3(1,0.9,1));
+	headBobble = new Animation<float>(&bobbleVal);
+	headBobble->loopType = Animation<float>::LoopType::kLOOP;
+	headBobbleTween1 = new Tween<float>(0.15f,-0.005f,Easing::kEASE_IN_OUT_CUBIC);
+	headBobbleTween2 = new Tween<float>(0.15f,0.005f,Easing::kEASE_IN_OUT_CUBIC);
+	headBobble->startValue= 0.f;
+	headBobble->tweens.push_back(headBobbleTween1);
+	headBobble->tweens.push_back(headBobbleTween2);
+	headBobble->hasStart = true;
 
+	bobbleInterpolation = 0.f;
 
+	/*easeIntoBobble = new Animation<float>(&easeIntoBobbleVal);
+	easeIntoBobbleTween1 = new Tween<float>(0.25f,0.05f,Easing::kEASE_IN_CUBIC);
+	easeIntoBobble->startValue=0.f;
+	easeIntoBobble->tweens.push_back(easeIntoBobbleTween1);
+	easeIntoBobble->hasStart = true;
+
+	easeOutOfBobble = new Animation<float>(&easeOutOfBobbleVal);
+	easeOutOfBobbleTween1 = new Tween<float>(0.25f,-0.05f,Easing::kEASE_OUT_CUBIC);
+	easeOutOfBobble->startValue=0.f;
+	easeOutOfBobble->tweens.push_back(easeOutOfBobbleTween1);
+	easeOutOfBobble->hasStart = true;*/
+
+	this->maxVelocity = btVector3(-1,-1,-1);
+
+	
+	playerCamera->interpolation = 0.8f;
 };
 
 void Player::update(Step * _step){
-	float playerSpeed = 3.f;
+	float playerSpeed = 0.1f;
 	float mass = 1;
 	float initSpeed = 2.0f;
+	float sprintSpeed = 5.0f;
 	btVector3 curVelocity = this->body->getLinearVelocity();
 	float xVelocity = curVelocity[0];
 	float zVelocity = curVelocity[2];
+	
+	btVector3 b = this->body->getWorldTransform().getOrigin();
+	
+	if(playerCamera->pitch > 80){
+		playerCamera->pitch = 80;
+	}
+	else if(playerCamera->pitch < -80){
+		playerCamera->pitch = -80;
+	}
 
 	//mouseCam->parents.at(0)->translate(player->getWorldPos() + glm::vec3(0, 0, player->parents.at(0)->getScaleVector().z*1.25f), false);
 	//mouseCam->lookAtOffset = glm::vec3(0, 0, -player->parents.at(0)->getScaleVector().z*0.25f);
@@ -90,7 +127,7 @@ void Player::update(Step * _step){
 	}if (keyboard->keyDown(GLFW_KEY_D)){
 		movement += right;
 	}if (keyboard->keyJustUp(GLFW_KEY_SPACE)){
-			movement += glm::vec3(0,10,0);
+			movement += glm::vec3(0,100,0);
 	}
 
 
@@ -104,27 +141,51 @@ void Player::update(Step * _step){
 		mouse->translate(glm::vec2(x2, y2));
 	}
 	
+	
+	
+
 	float movementMag = glm::length(movement);
+
+	
+
 	if(movementMag > 0){
+
 		//movement = movement/movementMag * playerSpeed * mass;
 		movement = movement/movementMag * playerSpeed * mass;
 		float initXSpeed = (movement/movementMag)[0]*initSpeed;
 		float initZSpeed = (movement/movementMag)[2]*initSpeed;
 
+		float sprintXSpeed = (movement/movementMag)[0]*sprintSpeed;
+		float sprintZSpeed = (movement/movementMag)[2]*sprintSpeed;
+
 		this->body->activate(true);
-		this->body->applyCentralImpulse(btVector3(initXSpeed+movement.x, movement.y*5, initZSpeed+movement.z));
-		this->body->applyDamping(btScalar(0));
-		this -> maxVelocity = btVector3(-1,-1,-1);
+		if(keyboard->keyDown(GLFW_KEY_LEFT_SHIFT)){
+			if(bobbleInterpolation<2){
+				bobbleInterpolation += 0.1f;
+			}
+			this->body->applyCentralImpulse(btVector3(sprintXSpeed+movement.x, movement.y*100, sprintZSpeed+movement.z));
+		}
+		else{
+			if(bobbleInterpolation<1){
+				bobbleInterpolation += 0.1f;
+			}
+			this->body->applyCentralImpulse(btVector3(initXSpeed+movement.x, movement.y*100, initZSpeed+movement.z));
+		}
+		//this -> maxVelocity = btVector3(-1,-1,-1);
+		
 	}
 
 
 	else if(movementMag <= 0){
 		float slideVal = 10.0f;
 		this->body->activate(true);
-		
-
 		this->body->applyCentralImpulse(btVector3(xVelocity*-0.2,0,zVelocity*-0.2));
-
+		if(bobbleInterpolation > 0){
+			bobbleInterpolation -= 0.1f;
+		}
+		else{
+			bobbleInterpolation = 0.f;
+		}
 		/*float curVel = std::max(abs(xVelocity), abs(zVelocity));
 		if(curVel > slideVal){
 			if(xVelocity > zVelocity){
@@ -141,7 +202,10 @@ void Player::update(Step * _step){
 		}*/
 	}
 
-
+	if(curVelocity[1] <= 0.01f){
+			headBobble->update(_step);
+			playerCamera->firstParent()->translate(b.x(), bobbleVal*bobbleInterpolation+b.y(), b.z(), false);
+		}
 
 
 	// mouse interaction with world objects
