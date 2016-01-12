dce7d281ed9b6241bb0513ca90188c95755fafc2
Ryan Bluth <ryanbluth93@gmail.com>
2016-01-11 22:50:16 -0500

    Furniture loaded into single mesh

--- Party Darling/Source/Party Darling/src/PD_FurnitureComponent.cpp
+++ Party Darling/Source/Party Darling/src/PD_FurnitureComponent.cpp
@@ -3,13 +3,6 @@
 #include <PD_FurnitureComponent.h>
 #include <Resource.h>
 
-PD_FurnitureConnector::PD_FurnitureConnector(Json::Value _jsonDef) {
-	description =_jsonDef.get("description", "UNDEFINED").asString();
-	positionX = _jsonDef.get("positionX", 0.0f).asFloat();
-	positionY = _jsonDef.get("positionY", 0.0f).asFloat();
-	positionZ = _jsonDef.get("positionZ", 0.0f).asFloat();
-}
-
 PD_FurnitureComponent::PD_FurnitureComponent(Json::Value _jsonDef) {
 	id = _jsonDef.get("id", -1).asInt();
 	type = _jsonDef.get("type", -1).asString();
@@ -20,6 +13,9 @@ PD_FurnitureComponent::PD_FurnitureComponent(Json::Value _jsonDef) {
 	}
 
 	for(auto jsonObj : _jsonDef["connectors"]) {
-		connectors.push_back(new PD_FurnitureConnector(jsonObj));
+		connectors[jsonObj.get("description", "UNDEFINED").asString()] = glm::vec3(
+			jsonObj.get("positionX", 0.0f).asFloat(),
+			jsonObj.get("positionY", 0.0f).asFloat(),
+			jsonObj.get("positionZ", 0.0f).asFloat());
 	}
 }
--- Party Darling/Source/Party Darling/src/PD_FurnitureComponentDefinition.cpp
+++ Party Darling/Source/Party Darling/src/PD_FurnitureComponentDefinition.cpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <PD_FurnitureComponentDefinition.h>
+#include <MeshEntity.h>
 
 PD_FurnitureComponentDefinition::PD_FurnitureComponentDefinition(Json::Value _jsonDef) {
 	componentType = _jsonDef.get("componentType", "UNDEFINED").asString();
@@ -12,9 +13,28 @@ PD_FurnitureComponentDefinition::PD_FurnitureComponentDefinition(Json::Value _js
 	}
 }
 
-PD_FurnitureComponent * PD_FurnitureComponentDefinition::build(PD_FurnitureComponentContainer * _componentContainer) {
-	PD_FurnitureComponent * comp = _componentContainer->getComponentForType(componentType);
-	return comp;
+MeshEntity * PD_FurnitureComponentDefinition::buildChildren(PD_FurnitureComponentContainer* _componentContainer) const {
+	PD_FurnitureComponent * component = _componentContainer->getComponentForType(componentType);
+	MeshEntity * ent = new MeshEntity(component->mesh);
+	std::vector<MeshEntity *>meshes;
+	for(auto def : outComponents) {
+		MeshEntity * mesh = def->buildChildren(_componentContainer);
+		ent->childTransform->addChild(mesh);
+		if(component->connectors.find(def->componentType) != component->connectors.end()) {
+			mesh->firstParent()->translate(component->connectors.at(def->componentType));
+		}
+		meshes.push_back(def->buildChildren(_componentContainer));
+	}
+	for(auto mesh : meshes) {
+		int offset = ent->mesh->vertices.size();
+		int indOffet = ent->mesh->indices.size();
+		mesh->freezeTransformation();
+		ent->mesh->vertices.insert(ent->mesh->vertices.end(), mesh->mesh->vertices.begin(), mesh->mesh->vertices.end());
+		ent->mesh->indices.insert(ent->mesh->indices.end(), mesh->mesh->indices.begin(), mesh->mesh->indices.end());
+		for(unsigned long int i = indOffet; i < ent->mesh->indices.size(); ++i) {
+			ent->mesh->indices.at(i) = ent->mesh->indices.at(i) + offset;
+		}
+	}
+	ent->freezeTransformation();
+	return ent;
 }
-
-
