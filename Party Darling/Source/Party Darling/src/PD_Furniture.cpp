61feafb8e12211fdc797e2ee6612f0eb8db5634f
Ryan Bluth <ryanbluth93@gmail.com>
2016-01-12 20:15:16 -0500

    Added some comments to the current furniture generation code

--- Party Darling/Source/Party Darling/src/PD_Furniture.cpp
+++ Party Darling/Source/Party Darling/src/PD_Furniture.cpp
@@ -11,26 +11,48 @@ PD_Furniture::PD_Furniture(Shader * _shader, PD_FurnitureDefinition * _def, PD_F
 	MeshEntity(new TriMesh(), _shader)
 {
 	std::vector<MeshEntity *>meshes;
+	
+	// Loop through each of the root components of the furniture definition
 	for(unsigned long int i = 0; i < _def->components.size(); ++i) {
+		// Get a random component for the current component type
 		auto component = _components->getComponentForType(_def->components.at(i)->componentType);
-		meshes.push_back(new MeshEntity(component->mesh));
+		// Create a new mesh so we don't affect the mesh store in the components container
+		TriMesh * tempMesh = new TriMesh();
+		tempMesh->vertices.insert(tempMesh->vertices.end(), component->mesh->vertices.begin(), component->mesh->vertices.end());
+		tempMesh->indices.insert(tempMesh->indices.end(), component->mesh->indices.begin(), component->mesh->indices.end());
+		// Add the temp mesh into meshes for the mesh combine which will occur later
+		meshes.push_back(new MeshEntity(tempMesh));
+		// Loop through each of the out components for the current component
 		for(unsigned long int j = 0; j < _def->components.at(i)->outComponents.size(); ++j) {
+			// Build that component and its children and add the returned mesh entity to the meshes
+			// array for the mesh combinme which will occur later
 			 MeshEntity * ent = _def->components.at(i)->outComponents.at(j)->buildChildren(
 				 _components,  _def->components.at(i)->multipliers.at(j), 
 				 component->connectors[_def->components.at(i)->outComponents.at(j)->componentType]);
 			 meshes.push_back(ent);
 		}
 	}
+	
+	// Loop through all the meshes which were created
 	for(auto m : meshes) {
+		// Create a indice offset and vert offset
+		// Since each mesh has indices for its verts we need to take into account
+		// How many vertices have already been added to the combined mesh and 
+		// offset the index value by that amount
 		int indOffset = mesh->indices.size();
-		int offset = mesh->vertices.size();
+		int vertOffset = mesh->vertices.size();
 		mesh->vertices.insert(mesh->vertices.end(), m->mesh->vertices.begin(), m->mesh->vertices.end());
 		mesh->indices.insert(mesh->indices.end(), m->mesh->indices.begin(), m->mesh->indices.end());
 		for(unsigned long int i = indOffset; i < mesh->indices.size(); ++i) {
-			mesh->indices.at(i) = mesh->indices.at(i) + offset;
+			mesh->indices.at(i) = mesh->indices.at(i) + vertOffset;
 		}
 	}
+
+	// Make the mesh dirty since the verts have changed
+	// May be redunant but do it as a safe guard
 	mesh->dirty = true;
+
+	// Cleanup all the meshes we created
 	for(auto m : meshes) {
 		childTransform->removeChild(m->firstParent());
 		delete m;
--- Party Darling/Source/Party Darling/src/PD_FurnitureComponentDefinition.cpp
+++ Party Darling/Source/Party Darling/src/PD_FurnitureComponentDefinition.cpp
@@ -18,37 +18,69 @@ PD_FurnitureComponentDefinition::PD_FurnitureComponentDefinition(Json::Value _js
 }
 
 MeshEntity * PD_FurnitureComponentDefinition::buildChildren(PD_FurnitureComponentContainer * _componentContainer, int _multiplier, std::vector<glm::vec3> _positions) const {
+	
+	// The number of positions should match the multiplier
+	assert(_positions.size() == _multiplier);
+
+	// An array of meshes which will be combined into a final mesh
 	std::vector<MeshEntity *>meshes;
+	
+	// The final mesh entity which is populated with verts and returned
 	MeshEntity * ent = new MeshEntity(new TriMesh());
+	
+	// Get a component for the component type - ex : Leg, Seat, Etc
 	PD_FurnitureComponent * component = _componentContainer->getComponentForType(componentType);
-	for(unsigned long int y = 0; y < _multiplier; y++){
+
+	// We need to loop for the value of _multiplier as there may be multiple of one component
+	// for example a chair seat has 4 legs - so the leg multiplier is 4
+	for(unsigned long int multIdx = 0; multIdx < _multiplier; multIdx++){
+		// Create a temporary tri mesh and copy in the verts and indices from the component mesh
+		// We do this so that we don't affect the original mesh when freezing transformations
+		// As this will screw things up here, as well as outside the scope of this class
 		TriMesh * tempMesh = new TriMesh();
 		tempMesh->vertices.insert(tempMesh->vertices.end(), component->mesh->vertices.begin(), component->mesh->vertices.end());
 		tempMesh->indices.insert(tempMesh->indices.end(), component->mesh->indices.begin(), component->mesh->indices.end());
 		MeshEntity * compMesh = new MeshEntity(tempMesh);
-		compMesh->meshTransform->translate(_positions[y]);
+
+		// Translate the MeshEntity created from the temporary mesh
+		// by the value located in _positions at the current multiplier value
+		compMesh->meshTransform->translate(_positions[multIdx]);
+		// Freeze the transformations
 		compMesh->freezeTransformation();
+		// Add it to the list of meshes
 		meshes.push_back(compMesh);
-		for(unsigned long int x = 0; x < outComponents.size(); ++x){
-			if(component->connectors.find(outComponents.at(x)->componentType) != component->connectors.end()) {
-				MeshEntity * mesh = outComponents.at(x)->buildChildren(
-					_componentContainer, multipliers[x], 
-					component->connectors[outComponents.at(x)->componentType]);
+		// Loop through each of the out components for this component definition
+		for(unsigned long int compIdx = 0; compIdx < outComponents.size(); ++compIdx){
+			// Make sure we have the connector data for the 
+			if(component->connectors.find(outComponents.at(compIdx)->componentType) != component->connectors.end()) {
+				// Build the component and its children
+				MeshEntity * mesh = outComponents.at(compIdx)->buildChildren(
+					_componentContainer, multipliers[compIdx], 
+					component->connectors[outComponents.at(compIdx)->componentType]);
+				// Add the resulting mesh to the meshes vector
 				meshes.push_back(mesh);
-				mesh->freezeTransformation();
+			}else {
+				ST_LOG_ERROR_V("Invalid connector data found when bulding furniture component");
 			}
 		}
-		for(auto mesh : meshes) {
-			int offset = ent->mesh->vertices.size();
-			int indOffet = ent->mesh->indices.size();
-			ent->mesh->vertices.insert(ent->mesh->vertices.end(), mesh->mesh->vertices.begin(), mesh->mesh->vertices.end());
-			ent->mesh->indices.insert(ent->mesh->indices.end(), mesh->mesh->indices.begin(), mesh->mesh->indices.end());
-			for(unsigned long int i = indOffet; i < ent->mesh->indices.size(); ++i) {
-				ent->mesh->indices.at(i) = ent->mesh->indices.at(i) + offset;
-			}
-		}
-		meshes.clear();
 	}
-	ent->freezeTransformation();
+	for(auto mesh : meshes) {
+		// Create a indice offset and vert offset
+		// Since each mesh has indices for its verts we need to take into account
+		// How many vertices have already been added to the combined mesh and 
+		// offset the index value by that amount
+		int vertOffset = ent->mesh->vertices.size();
+		int indOffet = ent->mesh->indices.size();
+		ent->mesh->vertices.insert(ent->mesh->vertices.end(), mesh->mesh->vertices.begin(), mesh->mesh->vertices.end());
+		ent->mesh->indices.insert(ent->mesh->indices.end(), mesh->mesh->indices.begin(), mesh->mesh->indices.end());
+		for(unsigned long int i = indOffet; i < ent->mesh->indices.size(); ++i) {
+			ent->mesh->indices.at(i) = ent->mesh->indices.at(i) + vertOffset;
+		}
+	}
+	// Delete temporary meshes
+	for(auto m : meshes) {
+		delete m;
+	}
+	// Returned the MeshEntity with the combined mesh
 	return ent;
 }
