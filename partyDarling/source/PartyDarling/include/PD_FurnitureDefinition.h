a0f9f4579ccd7cde8d0831f3de0979a66bfbb76b
Ryan Bluth <ryanbluth93@gmail.com>
2016-02-05 13:30:28 -0500

    Parsing for new furniture stuff

--- partyDarling/source/PartyDarling/include/PD_FurnitureDefinition.h
+++ partyDarling/source/PartyDarling/include/PD_FurnitureDefinition.h
@@ -5,13 +5,41 @@
 
 class PD_FurnitureComponentDefinition;
 
+enum class PD_Side {
+	kFRONT,
+	kBACK,
+	kBOTTOM,
+	kTOP,
+	kLEFT,
+	kRIGHT,
+	kNONE
+}; 
+
+
+class PD_FurnitureSides{
+public:
+	PD_Side front;
+	PD_Side back;
+	PD_Side left;
+	PD_Side right;
+	PD_Side top;
+
+	explicit PD_FurnitureSides(Json::Value _jsonDef);
+
+	static PD_Side fromString(std::string _side);
+};
+
 class PD_FurnitureDefinition {
 public:
 
 	std::string type;
 	float mass;
+	float padding;
 	bool detailedCollider;
 	std::vector<PD_FurnitureComponentDefinition *> components;
+	PD_FurnitureSides sides;
+	std::vector<std::string> parents;
+	std::vector<std::string> roomTypes;
 
 	explicit PD_FurnitureDefinition(Json::Value _jsonDef);
 };
--- partyDarling/source/PartyDarling/src/PD_FurnitureDefinition.cpp
+++ partyDarling/source/PartyDarling/src/PD_FurnitureDefinition.cpp
@@ -3,12 +3,40 @@
 #include <PD_FurnitureDefinition.h>
 #include <PD_FurnitureComponentDefinition.h>
 
+PD_FurnitureSides::PD_FurnitureSides(Json::Value _jsonDef) {
+	left = fromString(_jsonDef.get("LEFT", "NONE").asString());
+	right = fromString(_jsonDef.get("RIGHT", "NONE").asString());
+	top = fromString(_jsonDef.get("TOP", "NONE").asString());
+	front = fromString(_jsonDef.get("FRONT", "NONE").asString());
+	back = fromString(_jsonDef.get("BACK", "NONE").asString());
+}
+
+PD_Side PD_FurnitureSides::fromString(std::string _side) {
+	if(_side == "FRONT")	return PD_Side::kFRONT;
+	if(_side == "BACK")		return PD_Side::kBACK;
+	if(_side == "LEFT")		return PD_Side::kLEFT;
+	if(_side == "RIGHT")	return PD_Side::kRIGHT;
+	if(_side == "TOP")		return PD_Side::kTOP;
+	if(_side == "BOTTOM")	return PD_Side::kBOTTOM;
+	return PD_Side::kNONE;
+}
+
 PD_FurnitureDefinition::PD_FurnitureDefinition(Json::Value _jsonDef) :
 	type(_jsonDef.get("type", "UNDEFINED").asString()),
 	mass(_jsonDef.get("mass", 0.f).asFloat()),
-	detailedCollider(_jsonDef.get("detailedCollider", true).asBool())
+	padding(_jsonDef.get("padding", 0.f).asFloat()),
+	detailedCollider(_jsonDef.get("detailedCollider", true).asBool()),
+	sides(PD_FurnitureSides(_jsonDef["sides"]))
 {
 	for(auto outCompJson : _jsonDef["components"]) {
 		components.push_back(new PD_FurnitureComponentDefinition(outCompJson));								
+	}
+
+	for(auto parent : _jsonDef["parents"]) {
+		parents.push_back(parent.asString());
+	}
+
+	for(auto type : _jsonDef["roomTypes"]) {
+		roomTypes.push_back(type.asString());
 	}
 }
