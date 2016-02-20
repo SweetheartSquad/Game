9c751e1aa581fa6b6fc10e35597dc9c62c397a6e
Ryan Bluth <ryanbluth93@gmail.com>
2016-02-19 20:36:02 -0500

    scenario order

--- partyDarling/source/PartyDarling/include/PD_Scenario.h
+++ partyDarling/source/PartyDarling/include/PD_Scenario.h
@@ -9,11 +9,21 @@ enum ScenarioType {
 	kPLOT		= 2,
 }; 
 
+enum ScenarioOrder {
+	kRANDOM        = 0,
+	kBEGINNING     = 1,
+	kMIDDLE_ONE    = 2,
+	kMIDDLE_TWO    = 3,
+	kMIDDLE_THREE  = 4,
+	kEND           = 5
+};
+
 class PD_Scenario : public Scenario {
 public:	
-	std::string  name;
-	std::string  description;
-	ScenarioType scenarioType;
+	std::string   name;
+	std::string   description;
+	ScenarioType  scenarioType;
+	ScenarioOrder scenarioOrder;
 
 	explicit PD_Scenario(std::string _jsonSrc);
 };
--- partyDarling/source/PartyDarling/src/PD_Scenario.cpp
+++ partyDarling/source/PartyDarling/src/PD_Scenario.cpp
@@ -13,6 +13,7 @@ PD_Scenario::PD_Scenario(std::string _jsonSrc) : Scenario(_jsonSrc),
 	name		 = root.get("name", "NO_NAME").asString();
 	description  = root.get("description", "NO_DESCRIPTION").asString();
 	scenarioType = static_cast<ScenarioType>(root.get("type", 0).asInt());
+	order		 = static_cast<ScenarioType>(root.get("order", 0).asInt());
 }
 
 
