2da3064391e9a12e64ff167e93568782ee0e3e94
Ryan Bluth <ryanbluth93@gmail.com>
2016-03-03 13:19:43 -0500

    Oops

--- partyDarling/source/PartyDarling/src/PD_Scene_Main.cpp
+++ partyDarling/source/PartyDarling/src/PD_Scene_Main.cpp
@@ -642,7 +642,6 @@ void PD_Scene_Main::pickScenarios(){
 	//activeScenarios.push_back(new PD_Scenario("assets/scenario-external-1.json"));
 	//activeScenarios.push_back(new PD_Scenario("assets/scenario-external-2.json"));
 	activeScenarios.push_back(new PD_Scenario("assets/scenario-intro.json"));
-	activeScenarios.push_back(new PD_Scenario("assets/test-scenario.json"));
 	//activeScenarios.push_back(new Scenario("assets/scenario-external-3.json"));
 
 	// set event managers on selected scenarios as children of the global scenario
