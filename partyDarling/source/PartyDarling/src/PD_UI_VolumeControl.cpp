636ed5af56ece3bc67ee70c19ee5dae9a2b992d4
seleb <sean.s.leblanc@gmail.com>
2016-03-04 19:33:11 -0500

    volume uses current values as defaults to avoid changing them

--- partyDarling/source/PartyDarling/src/PD_UI_VolumeControl.cpp
+++ partyDarling/source/PartyDarling/src/PD_UI_VolumeControl.cpp
@@ -18,7 +18,7 @@ PD_UI_VolumeControl::PD_UI_VolumeControl(BulletWorld * _world, Shader * _textSha
 
 void PD_UI_VolumeControl::addSlider(std::string _text, float * _target){
 	TextLabel * label = new TextLabel(world, PD_ResourceManager::scenario->getFont("FONT")->font, textShader);
-	SliderController * slider = new SliderController(world, _target, 1.f, 0.f, 2.f);
+	SliderController * slider = new SliderController(world, _target, *_target, 0.f, 2.f);
 
 	addChild(label);
 	addChild(slider);
