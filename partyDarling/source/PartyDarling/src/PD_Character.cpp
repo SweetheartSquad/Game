7a8bc09d601e54edb45aef31547efaf84ecc5d66
seleb <sean.s.leblanc@gmail.com>
2016-03-12 16:40:44 -0500

    dumb fix for not randomly selecting dead animation
    replace this with a proper system later

--- partyDarling/source/PartyDarling/src/PD_Character.cpp
+++ partyDarling/source/PartyDarling/src/PD_Character.cpp
@@ -443,11 +443,21 @@ CharacterRenderer::~CharacterRenderer(){
 void CharacterRenderer::setAnimation(std::string _name) {
 	std::string selectedAnim = _name;
 	if(_name == "RANDOM"){
-		auto it = PD_ResourceManager::characterAnimations.begin();
-		int idx = sweet::NumberUtils::randomInt(0, PD_ResourceManager::characterAnimations.size() - 1);
-		std::advance(it, idx);
-		setAnimation(it->second);
-		selectedAnim = it->first;
+		bool validAnimation = false;
+		std::vector<PD_CharacterAnimationStep> anim;
+		std::string animKey;
+		while(!validAnimation){
+			auto it = PD_ResourceManager::characterAnimations.begin();
+			int idx = sweet::NumberUtils::randomInt(0, PD_ResourceManager::characterAnimations.size() - 1);
+			std::advance(it, idx);
+			animKey = it->first;
+			if(animKey != "DEAD"){
+				anim = it->second;
+				validAnimation = true;
+			}
+		}
+		setAnimation(anim);
+		selectedAnim = animKey;
 	}else {
 		if(PD_ResourceManager::characterAnimations.find(_name) != PD_ResourceManager::characterAnimations.end()) {
 			setAnimation(PD_ResourceManager::characterAnimations[_name]);
