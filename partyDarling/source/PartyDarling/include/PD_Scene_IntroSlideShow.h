0696827db831da9b1ded9e3715e7965c10b63f6a
Emma Thurlow <ecthurlow@rogers.com>
2016-03-21 11:44:42 -0400

    Animate envelope in intro slideshow

--- partyDarling/source/PartyDarling/include/PD_Scene_IntroSlideShow.h
+++ partyDarling/source/PartyDarling/include/PD_Scene_IntroSlideShow.h
@@ -3,10 +3,13 @@
 #include <Scene_SlideShow.h>
 
 class ComponentShaderText;
+class Timeout;
 
 class PD_Scene_IntroSlideShow : public Scene_SlideShow{
 private:
 	ComponentShaderText * textShader;
+	Timeout * changeSlideTimeout;
+	bool isForwads;
 public:
 	PD_Scene_IntroSlideShow(Game * _game);
 	~PD_Scene_IntroSlideShow();
