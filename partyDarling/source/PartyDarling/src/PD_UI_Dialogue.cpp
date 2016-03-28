8b1516a5617a4bcde2b21080ef78c67f2421d877
seleb <sean.s.leblanc@gmail.com>
2016-03-27 23:45:03 -0400

    removed test dialogue text

--- partyDarling/source/PartyDarling/src/PD_UI_Dialogue.cpp
+++ partyDarling/source/PartyDarling/src/PD_UI_Dialogue.cpp
@@ -40,7 +40,6 @@ PD_UI_Dialogue::PD_UI_Dialogue(BulletWorld * _world, PD_UI_Bubble * _uiBubble) :
 	vl2->addChild(text);
 	vl2->setPadding(0.03f, 0.f);
 	text->setRationalWidth(1.f, vl2);
-	text->setText("test");
 	text->verticalAlignment = kMIDDLE;
 	background->setVisible(false);
 
